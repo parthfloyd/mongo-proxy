3457f1cfa03c471c759019a615e80a9957d496ec
cash1998 <31826543+cash1998@users.noreply.github.com>
2024-06-09 16:32:42 -0700

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -37,4 +37,5 @@ class MongoDB:
 
     async def delete_document(self, collection_name: str, document_id: str):
         collection = self.db[collection_name]
+        print("We rollin'.")
         await collection.delete_one({"_id": ObjectId(document_id)})
